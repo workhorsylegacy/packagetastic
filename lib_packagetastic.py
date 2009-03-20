7b678af1e35833c59cd39c0709b6db3f5da6cbe3
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-03-20 12:22:39 -0700

    Fixed the build failure of our first Fedora pure python library.

--- lib_packagetastic.py
+++ lib_packagetastic.py
@@ -221,6 +221,15 @@ fi"""
 	def __filter_hash_for_fedora(self, retval):
 		if self._build_method == 'c configure make':
 
+			retval['build_arch'] = 'i386'
+			retval['build'] = \
+"""%configure
+make %{?_smp_mflags}"""
+
+			retval['check'] = \
+"""cd tests
+make check-TESTS"""
+
 			retval['install'] = substitute_strings(
 """rm -rf $RPM_BUILD_ROOT
 make install DESTDIR=$RPM_BUILD_ROOT
@@ -237,19 +246,27 @@ rm -f $RPM_BUILD_ROOT%{_infodir}/dir
 
 		elif self._build_method == 'pure python library':
 
-			retval['build_requirements'].append('python')
+			retval['build_requirements'].append('python-devel')
 			retval['install_requirements'].append('python')
+			retval['build_arch'] = 'noarch'
+			retval['build'] = "%{__python} setup.py build"
+			retval['check'] = ''
 
 			retval['install'] = substitute_strings(
-"""install -d %{python_sitelib}/#{name}
-install -pm 0644 #{name}.py %{python_sitelib}/#{name}/""", retval)
+"""rm -rf $RPM_BUILD_ROOT
+%{__python} setup.py install -O1 --skip-build --root $RPM_BUILD_ROOT
+
+
+# Correct some permissions
+find examples -type f -exec chmod a-x \{\} \;
+chmod 755 $RPM_BUILD_ROOT%{python_sitelib}/FSM.py""", retval)
+			# FIXME: This chmod should be a generic command that is part of the build
 
 			retval['files'] = substitute_strings(
 """
-%defattr(-,root,root,-)
-%dir %{python_sitelib}/#{name}
-%{python_sitelib}/#{name}/*.py
-%{python_sitelib}/#{name}/*.pyc""", retval)
+%defattr(-,root,root)
+%doc README doc examples LICENSE
+%{python_sitelib}/*""", retval)
 
 		return retval
 
@@ -518,7 +535,7 @@ def build_fedora(package):
 	f = open(os.path.expanduser('~/rpmbuild/SPECS/') + package.name + '.spec', 'w')
 
 	if package.build_method == 'pure python library':
-		f.write("%{!?python_sitelib: %global python_sitelib %(%{__python} -c " + \
+		f.write("%{!?python_sitelib: %define python_sitelib %(%{__python} -c " + \
 				"\"from distutils.sysconfig import get_python_lib; print get_python_lib()\")}\n\n")
 
 	f.write(substitute_strings(
@@ -529,12 +546,13 @@ Summary:        #{short_description}
 Group:          Development/Tools
 License:        #{license}
 URL:            #{homepage}
-Source0:        #{source}
+Source:         #{source}
 BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 
 
 BuildRequires: #{build_requirements}
 Requires: #{install_requirements}
+BuildArch: #{build_arch}
 
 
 %description
@@ -546,8 +564,7 @@ Requires: #{install_requirements}
 
 
 %build
-%configure
-make %{?_smp_mflags}
+#{build}
 
 
 %install
@@ -555,8 +572,7 @@ make %{?_smp_mflags}
 
 
 %check
-cd tests
-make check-TESTS
+#{check}
 
 
 %clean
@@ -583,12 +599,14 @@ rm -rf $RPM_BUILD_ROOT
 
 	# Create the rpm file
 	print "Building the rpm package ..."
+
 	commands.getoutput("rpmbuild -ba ~/rpmbuild/SPECS/" + package.name + ".spec")
 
 	print "Copying the rpm package to the packages directory ..."
 	if not os.path.isdir("packages"): os.mkdir("packages")
-	rpm = package.name + "-" + package.version + "-1.fc10.i386.rpm"
-	commands.getoutput("cp ~/rpmbuild/RPMS/i386/" + rpm + " packages/" + rpm)
+	arch = package.to_hash('fedora')['build_arch']
+	rpm = package.name + "-" + package.version + "-1.fc10." + arch + ".rpm"
+	commands.getoutput("cp ~/rpmbuild/RPMS/" + arch + "/" + rpm + " packages/" + rpm)
 	print "Done"
 
 
