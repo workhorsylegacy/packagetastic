95dfd768d091fdacb9a40eca291f03a5cec2e741
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-08-25 02:00:14 -0700

    Fixed breakage in fedora building due to changes in stem files.

--- distros/fedora.py
+++ distros/fedora.py
@@ -1,13 +1,17 @@
 
 
 class Builder(object):
-	def build(self, package):
+	def build(self, meta, packages, root_password, gpg_password):
 		# Setup the directories
 		print "Setting up the rpmdev directories ..."
 		commands.getoutput('rm -rf ~/rpmbuild')
 		commands.getoutput('rpmdev-setuptree')
 
 		setup_source_code(meta)
+
+		# Copy the source code to the build tree
+		print "Copying the source code ..."
+		commands.getoutput(substitute_strings("cp ../../sources/" + meta.source.split('/')[-1] + " ~/rpmbuild/SOURCES/" + meta.source.split('/')[-1], meta.to_hash()))
 
 		# FIXME: Figure out how %defattr(-,root,root,-) works
 		# Determine the params for %files
@@ -21,71 +25,70 @@ class Builder(object):
 			if os.path.isdir(doc):
 				params['docs'].append(doc)
 
-		if os.path.isfile('doc/' + package.name + '.1'):
+		if os.path.isfile('doc/' + meta.name + '.1'):
 			params['has_man1'] = True
-		elif os.path.isfile('man/' + package.name + '.1'):
+		elif os.path.isfile('man/' + meta.name + '.1'):
 			params['has_man1'] = True
 		else:
 			params['has_man1'] = False
 
-		if os.path.isfile('doc/' + package.name + '_config.5'):
+		if os.path.isfile('doc/' + meta.name + '_config.5'):
 			params['has_man5'] = True
-		elif os.path.isfile('man/' + package.name + '_config.5'):
+		elif os.path.isfile('man/' + meta.name + '_config.5'):
 			params['has_man5'] = True
 		else:
 			params['has_man5'] = False
 
 		params['has_bindir'] = False
-		if package.build_method == 'c application' or package.build_method == 'c library':
+		if meta.build_method == 'c application' or meta.build_method == 'c library':
 			params['has_bindir'] = True
 			params['import_sitelib'] = False
-		elif package.build_method == 'python library':
+		elif meta.build_method == 'python library':
 			params['has_bindir'] = False
 			params['import_sitelib'] = True
-		elif package.build_method == 'python application':
+		elif meta.build_method == 'python application':
 			params['has_bindir'] = True
 			params['import_sitelib'] = True
 
-		params['has_info'] = os.path.isfile('doc/' + package.name + '.info')
+		params['has_info'] = os.path.isfile('doc/' + meta.name + '.info')
 
 		params['has_lang'] = os.path.isdir('po')
 
-		params['has_desktop_file'] = os.path.isfile('data/' + package.name + '.desktop')
+		params['has_desktop_file'] = os.path.isfile('data/' + meta.name + '.desktop')
 
 		params['has_icons'] = os.path.isdir('data/icons')
 
 		# Create the spec file
 		os.chdir('../..')
 		print "Building the spec file ..."
-		commands.getoutput('touch ~/rpmbuild/SPECS/' + package.name + '.spec')
-		f = open(os.path.expanduser('~/rpmbuild/SPECS/') + package.name + '.spec', 'w')
-
 		# Write the custom parts of the spec file
 		fields = None
-		if package.build_method == 'c application' or package.build_method == 'c library':
-			fields = self.generate_fedora_install_for_c_configure_make(f, package, params)
-		elif package.build_method == 'python library':
-			fields = self.generate_fedora_install_for_pure_python_library(f, package, params)
-		elif package.build_method == 'python application':
-			fields = self.generate_fedora_install_for_pure_python_library(f, package, params)
+		if meta.build_method == 'c application' or meta.build_method == 'c library':
+			fields = self.generate_spec_file_for_c(meta, packages, params)
+		elif meta.build_method == 'python library':
+			fields = self.generate_spec_file_for_python_library(meta, packages, params)
+		elif meta.build_method == 'python application':
+			fields = self.generate_spec_file_for_python_library(meta, packages, params)
 		else:
-			print "Unknown build method '" + package.build_method + "'. Exiting ..."
+			print "Unknown build method '" + meta.build_method + "'. Exiting ..."
 			exit()
-
-		f.close()
 
 		# Create the rpm file
 		packagetastic_dir = commands.getoutput('pwd')
 		os.chdir(os.path.expanduser("~"))
-		print "Building the rpm package ..."
 
-		command = "rpmbuild -ba rpmbuild/SPECS/" + package.name + ".spec"
+		print "Building the rpm package ..."
+		command = "rpmbuild -ba rpmbuild/SPECS/" + meta.name + ".spec"
 		print commands.getoutput(command)
 		"""
 		child = pexpect.spawn(command, timeout=1200)
 
 		expected_lines = ["error: Failed build dependencies:\r\n" +
-							"[\W]*[\w|\d|\.|\-]* is needed by [\w|\d|\.|\-]*fc[\d]*.src",
+							"[\W]*[\w|\d|\.|\-]* is needed by [\w|\d|\.|\-]*fc[\d]*.src", 
+
+							"error: File /home/[\w|\d|\_]*/rpmbuild/SOURCES/" + 
+							meta.name + "-" + meta.version + ".tar.gz:" + 
+							" No such file or directory", 
 
 							pexpect.EOF]
 
@@ -97,6 +100,8 @@ class Builder(object):
 
 			if result == 0:
 				print child.after
+			elif result == 1:
+				print child.after
 			elif result == len(expected_lines)-1:
 				still_reading = False
 
@@ -107,21 +112,23 @@ class Builder(object):
 		os.chdir(packagetastic_dir)
 		if not os.path.isdir("packages"): os.mkdir("packages")
 		arch = fields['build_arch']
-		rpm = package.name + "-" + package.version + "-1.fc11." + arch + ".rpm"
+		rpm = meta.name + "-" + meta.version + "-1.fc11." + arch + ".rpm"
 		commands.getoutput("cp ~/rpmbuild/RPMS/" + arch + "/" + rpm + " packages/" + rpm)
 		print "Done"
 
-	def generate_fedora_install_for_c_configure_make(self, f, package, params):
+	def generate_spec_file_for_c(self, meta, packages, params):
 		# Make additions to fields
-		fields = package.to_hash({
+		fields = meta.to_hash({
 				'build_arch' : 'i386', 
-				'files' : self.generate_file(package, params), 
-				'pre_and_post' : self.generate_pre_and_post_functions(package, params), 
-				'install' : self.generate_install_for_c_configure_make(package, params), 
-				'install_extra' : self.generate_install_extras(package, params), 
-				'changelog' : self.generate_changelog(package)
+				'files' : self.generate_file(meta, params), 
+				'pre_and_post' : self.generate_pre_and_post_functions(meta, params), 
+				'install' : self.generate_install_for_c(meta, params), 
+				'install_extra' : self.generate_install_extras(meta, params), 
+				'changelog' : self.generate_changelog(meta), 
+				'install_requirements' : str.join(', ', packages[0].install_requirements)
 		})
 
+		f = open(os.path.expanduser('~/rpmbuild/SPECS/') + meta.name + '.spec', 'w')
 		f.write(substitute_strings(
 """Name:           #{name}
 Version:        #{version}
@@ -172,22 +179,24 @@ rm -rf %{buildroot}
 %changelog
 #{changelog}
 """, fields))
+		f.close()
 
 		return fields
 
-	def generate_fedora_install_for_pure_python_library(self, f, package, params):
+	def generate_spec_file_for_python_library(self, meta, packages, params):
 		# Make additions to fields
-		fields = package.to_hash({
+		fields = meta.to_hash({
 				'build_arch' : 'noarch', 
-				'build_requirements' : ['python-devel'] + package.build_requirements, 
-				'install_requirements' : ['python'] + package.install_requirements, 
-				'files' : self.generate_file(package, params), 
-				'pre_and_post' : self.generate_pre_and_post_functions(package, params), 
-				'install' : self.generate_install_for_pure_python_library(package, params), 
-				'install_extra' : self.generate_install_extras(package, params), 
-				'changelog' : self.generate_changelog(package)
+				'build_requirements' : ['python-devel'] + meta.build_requirements, 
+				'files' : self.generate_file(meta, params), 
+				'pre_and_post' : self.generate_pre_and_post_functions(meta, params), 
+				'install' : self.generate_install_for_python_library(meta, params), 
+				'install_extra' : self.generate_install_extras(meta, params), 
+				'changelog' : self.generate_changelog(meta), 
+				'install_requirements' : str.join(', ', packages[0].install_requirements + ['python'])
 		})
 
+		f = open(os.path.expanduser('~/rpmbuild/SPECS/') + meta.name + '.spec', 'w')
 		f.write(substitute_strings(
 """%{!?python_sitelib: %define python_sitelib %(%{__python} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")}
 
@@ -236,6 +245,7 @@ rm -rf %{buildroot}
 %changelog
 #{changelog}
 """, fields))
+		f.close()
 
 		return fields
 
@@ -273,7 +283,7 @@ fi
 		return retval
 
 
-	def generate_file(self, package, params):
+	def generate_file(self, meta, params):
 		fields = []
 		lang = ''
 
@@ -315,23 +325,22 @@ fi
 		'fields' : str.join('\n', fields)
 	})
 
-	def generate_install_for_pure_python_library(self, package, params):
+	def generate_install_for_python_library(self, package, params):
 		return \
-"""
-%install
+"""%install
 rm -rf %{buildroot}
 %{__python} setup.py install -O1 --skip-build --root %{buildroot}
 """
 
-	def generate_install_for_c_configure_make(self, package, params):
+	def generate_install_for_c(self, meta, params):
 		return \
-""" %install
+"""%install
 rm -rf %{buildroot}
 make install DESTDIR=%{buildroot}
 rm -f %{buildroot}%{_infodir}/dir
 """
 
-	def generate_install_extras(self, package, params):
+	def generate_install_extras(self, meta, params):
 		retval = ''
 
 		if params['has_lang'] == True:
@@ -350,29 +359,33 @@ desktop-file-install --dir=%{buildroot}%{_datadir}/applications data/%{name}.des
 
 		return retval
 
-	def generate_changelog(self, package):
+	def generate_changelog(self, meta):
 		# FIXME: These should be at the top.
 		from time import strftime
 		from email.utils import parsedate
 
 		# Create the changelog
 		changelog_body = ""
-		if package.changelog == None:
+		if meta.changelog == None:
 			print "The changelog is missing. Exiting ..."
 			exit()
 
-		reverse_entries = package.changelog[:]
+		reverse_entries = meta.changelog[:]
 		reverse_entries.reverse()
 		for item in reverse_entries:
 			item['time'] = time.strftime("%a %b %d %Y", parsedate(item['time']))
+
+			fields = meta.to_hash({
+					'item_version' : item['version'], 
+					'item_time' : item['time'], 
+					'item_text' : item['text']
+			})
 
 			entry = substitute_strings(
 """* #{item_time} - #{packager_email} - #{item_version}
 - #{item_text}
 
-""", package.to_hash({'item_version' : item['version'], 
-					'item_time' : item['time'], 
-					'item_text' : item['text']}))
+""", fields)
 
 			changelog_body = entry + changelog_body
 
