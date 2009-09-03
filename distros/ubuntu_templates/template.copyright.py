25a6f754b4d7b8ebf2734245951a7bb827907308
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-09-02 23:52:30 -0700

    Fixed a small bug in the ubuntu copyright files, that messed up the spacing of licenses.

--- distros/ubuntu_templates/template.copyright.py
+++ distros/ubuntu_templates/template.copyright.py
@@ -20,7 +20,7 @@ Copyright:
 
 License:
 
-${"    " + license_text.replace("\n", "    \n")}
+${"    " + license_text.replace("\n", "\n    ").rstrip()}
 
 The Debian packaging is (C) ${year}, ${packager_name} <${packager_email}> and
 is licensed under the ${license}, see above.
